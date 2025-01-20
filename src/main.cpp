c109e6cd4f9aeba8c5fb89ac544b0e9455fa09d0
pc0ln <pjcoln2004@gmail.com>
2025-01-19 22:36:13 -0700

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "server.h"
 
 int main() {
+    //Creates instance of server with local ip and port 8080 then starts
     server s = server("127.0.0.1", "8080");
     s.start();
 }
--- src/server.cpp
+++ src/server.cpp
@@ -16,15 +16,18 @@ server::server(char* ip_addr, char* port) {
     int status;
     int binded_socket;
 
+    // Sets address info struct to 0
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-
+    
+    //Gets address info basically fills struct
     if ((status = getaddrinfo("127.0.0.1", "8080", &hints, &result)) != 0) {
         std::cerr << "Get address info error: " << gai_strerror(status) << std::endl;
         exit(0);
     }
 
+    // Loops through linked list off addresses
     for (rp = result; rp != nullptr; rp = rp->ai_next) {
         // Just creating a socket
         server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,6 +40,7 @@ server::server(char* ip_addr, char* port) {
             break;
         }
     }
+    //Frees linked list
     freeaddrinfo(result);
 
     if (server_socket == -1) {
@@ -61,6 +65,7 @@ server::server(char* ip_addr, char* port) {
     std::cout << "Listening out on Port" << port << "\n";
 }
 
+// Starts serrver to accept clients in loop
 int server::start() {
     while(true) {
         // Accept
@@ -73,6 +78,8 @@ int server::start() {
         // Send Recieve
         std::cout << "Client connected." << std::endl;
 
+
+        //Multithreading using bind to bind to said instance of server
         std::thread client(std::bind(&server::handle_client, this, client_fd));
         client.detach(); 
     }
@@ -86,6 +93,7 @@ server::~server() {
 }
 
 void server::handle_client(int fd) {
+    //Fills recieved buffer from client
     char buffer[1024];
     ssize_t bytes_received = recv(fd, buffer, sizeof(buffer) - 1, 0);
     if (bytes_received > 0) {
@@ -96,19 +104,22 @@ void server::handle_client(int fd) {
         close(fd);
         return;
     }
-
+    // Send to handle request with extra buffer for response
     char response[1024];
     handle_request(buffer, response);
     std::cout << "response: " << response << '\n';
-
+    
+    // Sends response
     int bytes_sent = send(fd, response, strlen(response), 0);
     close(fd);
 }
 
 void server::handle_request(char* req, char* res) {
+    // If not get sends bad request
     if (strncmp(req, "GET", 3) != 0) {
         strncpy(res, "HTTP/1.1 400 Bad Request\r\n\r\n", 30);
     } else {
+        //Send ok response before filling body
         char* body = stpncpy(res, "HTTP/1.1 200 OK\r\n\r\n", 20);
         char* file = req + 4;
         *strchr(file, ' ') = 0;
@@ -117,6 +128,7 @@ void server::handle_request(char* req, char* res) {
 }
 
 void server::response_builder(const char* file, char* res_body, int offset) {
+    //If file isnt root the echos to client
     if (strcmp(file, "/") == 0) {
         int opened_fd = open("../src/index.html", O_RDONLY);
         if (opened_fd == -1) {
